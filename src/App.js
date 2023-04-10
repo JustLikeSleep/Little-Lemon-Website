2152e7509e50812c79bf5a080f29ba87f5bfe07f
JustKLikeSleep <74163510+JustKLikeSleep@users.noreply.github.com>
2023-04-10 12:15:49 -0400

    small update around index.html app.js & components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Nav from './Components/Nav';
+
+
 
 function App() {
   return (
-    <div className="App">
-      Learn React
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Header() {
+    return(
+        <img src="./assets/Logo.svg" alt="logo for site"></img>
+    );
+};
+
+export default Header
